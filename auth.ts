f45845a9215c5f8d0dade4264d9bdffb84222e03
ptakondrej <ptakondrej@spseplzen.cz>
2025-01-25 18:46:29 +0100

    Update auth.ts

--- auth.ts
+++ auth.ts
@@ -14,14 +14,13 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     GoogleProvider({
       clientId: process.env.AUTH_GOOGLE_ID as string,
       clientSecret: process.env.AUTH_GOOGLE_SECRET as string,
-      // access_type: "offline",
-      // authorization: {
-      //   params: {
-      //     prompt: "consent",
-      //     access_type: "offline",
-      //     response_type: "code",
-      //   },
-      // },
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
     }),
   ],
   callbacks: {
